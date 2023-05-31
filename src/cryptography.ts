308c3322d835d184343e749b5b952d1d6e9f460f
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-05-31 14:20:53 +0200

    feat: basic cryptography

---
+++ src/cryptography.ts
@@ -0,0 +1,18 @@
+import webCrypto from "./core";
+import { DEFAULT_CRYPTOGRAPHY } from "./constants";
+
+export type algorithmType = AlgorithmIdentifier | RsaOaepParams | AesCtrParams | AesCbcParams | AesGcmParams;
+
+export type cryptographyType = { algorithm?: algorithmType; key: CryptoKey; data: BufferSource };
+
+export async function decrypt(params: cryptographyType): Promise<ArrayBuffer> {
+  const alg = params.algorithm ?? DEFAULT_CRYPTOGRAPHY;
+  const arrayBuffer = await webCrypto.subtle.decrypt(alg, params.key, params.data);
+  return arrayBuffer;
+}
+
+export async function encrypt(params: cryptographyType): Promise<ArrayBuffer> {
+  const alg = params.algorithm ?? DEFAULT_CRYPTOGRAPHY;
+  const arrayBuffer = await webCrypto.subtle.encrypt(alg, params.key, params.data);
+  return arrayBuffer;
+}
